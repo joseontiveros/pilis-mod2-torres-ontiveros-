b84cc5f7537fbae5a07f0c88cf6c65113738cf99
Jose Ontiveros <josedanielontiveros@gmail.com>
2022-08-26 22:58:46 -0300

    Agrego seccion home, nostros y registro

--- main.js
+++ main.js
@@ -1,75 +1,99 @@
-window.addEventListener('load', ()=> {
-
-let temperaturaValor = document.getElementById('temperatura-valor');
-let temperaturaDescripcion = document.getElementById('temperatura-descripcion');
-
-let ubicacion = document.getElementById('ubicacion');
-let iconoAnimado = document.getElementById('icono-animado');
-
-let vientoVelocidad = document.getElementById('viento-velocidad');
-
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(posicion => {
-            
-            //coordenadas ciudad cultural
-            lon = -65.3309438515912;
-            lat = -24.18324151358606;
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=7e6fa103b201047e0873faf953ba25b6`            
-            fetch(url)
-                .then(response => { return response.json() })
-                .then ( data => {
-                    //temperatura
-                    let temp = Math.round(data.main.temp);
-                    temperaturaValor.textContent = `${temp} ºC`;
-                    //descripcion temperatura
-                    let desc = data.weather[0].description;
-                    temperaturaDescripcion.textContent = desc.toUpperCase();
-                    //ubicacion                    
-                    ubicacion.textContent = data.name;
-                    //viento                    
-                    vientoVelocidad.textContent = `${data.wind.speed} m/s`;
-                    console.log(data)
-                    //iconos animados clima
-                    console.log(data.weather[0].main);      
-                    //asignacion del icono segun descripcion              
-                    switch (data.weather[0].main) {
-                        case 'Thunderstorm':
-                          iconoAnimado.src='/assets/iconosAnimadosClima/thunder.svg'
-                          console.log('TORMENTA');
-                          break;
-                        case 'Drizzle':
-                          iconoAnimado.src='/assets/iconosAnimadosClima/rainy-2.svg'
-                          console.log('LLOVIZNA');
-                          break;
-                        case 'Rain':
-                          iconoAnimado.src='/assets/iconosAnimadosClima/rainy-7.svg'
-                          console.log('LLUVIA');
-                          break;
-                        case 'Snow':
-                          iconoAnimado.src='/assets/iconosAnimadosClima/snowy-6.svg'
-                            console.log('NIEVE');
-                          break;                        
-                        case 'Clear':
-                            iconoAnimado.src='/assets/iconosAnimadosClima/day.svg'
-                            console.log('LIMPIO');
-                          break;
-                        case 'Atmosphere':
-                          iconoAnimado.src='/assets/iconosAnimadosClima/weather.svg'
-                            console.log('ATMOSFERA');
-                            break;  
-                        case 'Clouds':
-                            iconoAnimado.src='/assets/iconosAnimadosClima/cloudy-day-1.svg'
-                            console.log('NUBES');
-                            break;  
-                        default:
-                          iconoAnimado.src='/assets/iconosAnimadosClima/cloudy-day-1.svg'
-                          console.log('por defecto');
-                      }
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        })
+function onClick (event) {
+    //anula el evento por defecto que tiene un boton dentro de un formulario
+    //cuando se presiona un boton dentro de un formulario lo que hace el evento es buscar
+    //dentro del formulario el sript asociado e intenta enviar informacion
+    //por eso se anula la accion normal del boton antes de enviar hay que procesar 
+    event.preventDefault();
+    
+    const mensaje = {
+      comercio: document.getElementById('comercio').value,
+      titular: document.getElementById('titular').value,
+      celular: document.getElementById('celular').value,
+      message: document.getElementById('message').value
     }
-})
+    console.log(mensaje);
+  
+  
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(mensaje),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => { 
+          console.log(json);
+          Swal.fire(
+              'Enviado',
+              'Gracias por tu comentario', 
+              'success'
+          );
+          cleanForm();
+          /* redirectUrl(); */
+      })
+      .catch((err) => console.log(err));
+  
+  }
+
+  function cleanForm() {
+    let formulario = document.getElementById('formulario');    
+    formulario.reset();    
+}
+function redirectUrl(){
+    window.location.href = "https://google.com";    
+}
+
+
+
+
+let boton = document.getElementById("enviar");
+//cuando se capture el click dek usuario, se ejecuta la funcion onclick
+boton.addEventListener("click", onClick);
+
+/* ------------------SECCION CLIMA---------------------------------------- */
+
+
+const api_key = 'eff45045e3d6f5585d9d08134ec413de';
+const fetchData = position => {
+  const { latitude, longitude } = position.coords;
+  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${-24.18324151358602}&lon=${-65.33130863237089}&units=metric&appid=${api_key}`)
+  .then(response => response.json())
+  .then(data => setWeatherData(data));   
+  }
+   
+const setWeatherData = data => {
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: Math.floor(data.main.temp),
+        date: getDate(),
+    }
+
+    Object.keys(weatherData).forEach( key => {
+        setTextContent(key, weatherData[key]);
+    });
+
+    cleanUp();
+}
+
+const cleanUp = () => {
+    let container = document.getElementById('container2');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none'; 
+    container.style.display = 'flex'; 
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${ ('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
+}
+
+const setTextContent = (element, text) => {
+    document.getElementById(element).textContent = text;
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData)
+}
